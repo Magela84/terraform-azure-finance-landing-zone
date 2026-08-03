9193864576cb14748ac7103b4180dc5cafbffd7e
Magela <magela8403@gmail.com>
2026-08-03 14:46:46 -0400

    Fix: Re-index tracking layout to explicitly capture infrastructure submodules

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"
-    storage_account_name = "sttfstateenterprise01"
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
-  }
-}
